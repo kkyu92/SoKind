86cbdf1e6829bf3c6f7ad6b62dc9bee4386b2085
kyusik <kyusikkyu@gmail.com>
2022-01-25 21:14:33 +0900

    [Feat] Edu - navController bundle

--- app/src/main/java/com/sokind/ui/EduNavActivity.kt
+++ app/src/main/java/com/sokind/ui/EduNavActivity.kt
@@ -2,13 +2,24 @@ package com.sokind.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.sokind.R
+import com.sokind.data.remote.edu.Edu
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class EduNavActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edu_nav)
+        val eduData = intent.getSerializableExtra("edu") as Edu
+        val bundle = bundleOf("edu" to eduData)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView_edu) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.setGraph(R.navigation.edu_nav, bundle)
     }
 }
--- app/src/main/java/com/sokind/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sokind/ui/home/HomeFragment.kt
@@ -1,21 +1,23 @@
 package com.sokind.ui.home
 
+import android.app.Activity
+import android.content.Intent
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jakewharton.rxbinding4.view.clicks
 import com.sokind.R
-import com.sokind.data.remote.edu.CsBase
-import com.sokind.data.remote.edu.CsDeep
 import com.sokind.data.remote.edu.Edu
 import com.sokind.data.remote.member.MemberInfo
 import com.sokind.databinding.FragmentHomeBinding
+import com.sokind.ui.EduNavActivity
 import com.sokind.ui.base.BaseFragment
-import com.sokind.util.Constants
-import com.sokind.util.ShowCsFragmentListener
+import com.sokind.util.*
 import com.sokind.util.adapter.BaseEduAdapter
 import com.sokind.util.adapter.DeepEduAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
@@ -23,10 +25,23 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val viewModel by viewModels<HomeViewModel>()
 
     private lateinit var showCsListener: ShowCsFragmentListener
+    private lateinit var showReportFragmentListener: ShowReportFragmentListener
     private lateinit var baseEduAdapter: BaseEduAdapter
     private lateinit var deepEduAdapter: DeepEduAdapter
 
     private lateinit var memberInfo: MemberInfo
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val go = it.data?.getStringExtra(Constants.MOVE_TO)
+            Timber.e("data: ${ it.data}")
+            Timber.e("go to : $go")
+            when(go) {
+                "cs" -> showToast("reload")
+                "report" -> showReportFragmentListener.showReportFragment()
+            }
+        }
+    }
 
     override fun init() {
         if (this::showCsListener.isInitialized) {
@@ -59,6 +74,46 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
                 tvDeepMore.visibility = View.VISIBLE
             }
         }
+        baseEduAdapter.setOnItemClickListener(object : OnEduItemClickListener {
+            override fun onEduItemClick(edu: Edu, pos: Int) {
+                when (edu.status) {
+                    1 -> {
+                        val dialog = BottomSheetDialog.newInstance(
+                            getString(R.string.alert),
+                            String.format(getString(R.string.alert_edu_fin, edu.title)),
+                            itemClick = { if (it) { startEdu(edu) } }
+                        )
+                        dialog.show(parentFragmentManager, dialog.tag)
+                    }
+                    2 -> {
+
+                    }
+                }
+            }
+        })
+        deepEduAdapter.setOnItemClickListener(object : OnEduItemClickListener {
+            override fun onEduItemClick(edu: Edu, pos: Int) {
+                when (edu.status) {
+                    1 -> {
+                        val dialog = BottomSheetDialog.newInstance(
+                            getString(R.string.alert),
+                            String.format(getString(R.string.alert_edu_fin, edu.title)),
+                            itemClick = { if (it) { startEdu(edu) } }
+                        )
+                        dialog.show(parentFragmentManager, dialog.tag)
+                    }
+                    2 -> {
+
+                    }
+                }
+            }
+        })
+    }
+
+    private fun startEdu(edu: Edu) {
+        val intent = Intent(requireContext(), EduNavActivity::class.java)
+        intent.putExtra("edu", edu)
+        startForResult.launch(intent)
     }
 
     private fun setBinding() {
