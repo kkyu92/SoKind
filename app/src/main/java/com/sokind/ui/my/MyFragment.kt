43024e415ac0d7e816acf786c4582eb5c8fe7cf3
kyusikkyu <kyusikkyu@gmail.com>
2022-02-24 17:43:28 +0900

    MyFragment 작업 - UI 수정 - 화면전환 및 api 작업 추가

--- app/src/main/java/com/sokind/ui/my/MyFragment.kt
+++ app/src/main/java/com/sokind/ui/my/MyFragment.kt
@@ -1,10 +1,16 @@
 package com.sokind.ui.my
 
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import com.jakewharton.rxbinding4.view.clicks
 import com.sokind.R
+import com.sokind.data.di.GlideApp
+import com.sokind.data.remote.member.MemberInfo
 import com.sokind.databinding.FragmentMyBinding
 import com.sokind.ui.base.BaseFragment
+import com.sokind.ui.my.info.InfoActivity
 import com.sokind.util.Constants
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.concurrent.TimeUnit
@@ -12,9 +18,34 @@ import java.util.concurrent.TimeUnit
 @AndroidEntryPoint
 class MyFragment : BaseFragment<FragmentMyBinding>(R.layout.fragment_my) {
     private val viewModel by viewModels<MyViewModel>()
+    private lateinit var user: MemberInfo
+
+    private val startForResultInfo =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                viewModel.getMe()
+            }
+        }
 
     override fun init() {
         setBinding()
+
+        viewModel.info.observe(viewLifecycleOwner, {
+            user = it
+            setData()
+        })
+    }
+
+    private fun setData() {
+        binding.apply {
+            GlideApp.with(requireContext())
+                .load(user.profileImg)
+                .error(R.drawable.icon_profile_default)
+                .into(ivProfile)
+            R.string.name
+            tvCsUserName.text = getString(R.string.name_form, user.memberName, user.positionName)
+            tvCsUserEnterprise.text = getString(R.string.enterprise_form, user.enterpriseName, user.storeName)
+        }
     }
 
     private fun setBinding() {
@@ -23,28 +54,28 @@ class MyFragment : BaseFragment<FragmentMyBinding>(R.layout.fragment_my) {
                 .clicks()
                 .throttleFirst(Constants.THROTTLE, TimeUnit.MILLISECONDS)
                 .subscribe({
-
+                    startView(InfoActivity::class.java)
                 },{ it.printStackTrace() })
 
             llMyCertification
                 .clicks()
                 .throttleFirst(Constants.THROTTLE, TimeUnit.MILLISECONDS)
                 .subscribe({
-
+//                    startView()
                 },{ it.printStackTrace() })
 
             llMyNotice
                 .clicks()
                 .throttleFirst(Constants.THROTTLE, TimeUnit.MILLISECONDS)
                 .subscribe({
-
+//                    startView()
                 },{ it.printStackTrace() })
 
             llMyQuestion
                 .clicks()
                 .throttleFirst(Constants.THROTTLE, TimeUnit.MILLISECONDS)
                 .subscribe({
-
+//                    startView()
                 },{ it.printStackTrace() })
 
             clMyUpdate
@@ -54,5 +85,11 @@ class MyFragment : BaseFragment<FragmentMyBinding>(R.layout.fragment_my) {
 
                 },{ it.printStackTrace() })
         }
+    }
+
+    private fun startView(className: Class<*>) {
+        val intent = Intent(requireContext(), className)
+        intent.putExtra("info", user)
+        startForResultInfo.launch(intent)
     }
 }
--- app/src/main/java/com/sokind/ui/my/MyViewModel.kt
+++ app/src/main/java/com/sokind/ui/my/MyViewModel.kt
@@ -1,12 +1,35 @@
 package com.sokind.ui.my
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.sokind.data.remote.member.MemberInfo
+import com.sokind.data.repository.member.MemberRepository
 import com.sokind.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
 @HiltViewModel
 class MyViewModel @Inject constructor(
-
+    private val repository: MemberRepository
 ) : BaseViewModel() {
-    // TODO: Implement the ViewModel
+    private val _info: MutableLiveData<MemberInfo> = MutableLiveData()
+    val info: LiveData<MemberInfo> = _info
+
+    init {
+        getMe()
+    }
+
+    fun getMe() {
+        compositeDisposable.add(
+            repository
+                .getMe()
+                .doOnSubscribe { showProgress() }
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnTerminate { hideProgress() }
+                .subscribe({
+                    _info.postValue(it)
+                }, { it.printStackTrace() })
+        )
+    }
 }
