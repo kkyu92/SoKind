df5f4b9a1da2f90b0c356610a7f9e8bf36e7c688
kyusikkyu <kyusikkyu@gmail.com>
2022-02-25 17:47:49 +0900

    Lottie 추가 - gradle - UI - loading json - dialogFragment singleton

---
+++ app/src/main/java/com/sokind/util/dialog/LottieDialogFragment.kt
@@ -0,0 +1,38 @@
+package com.sokind.util.dialog
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import com.sokind.R
+
+class LottieDialogFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        isCancelable = false
+        return inflater.inflate(R.layout.loading, container, false)
+    }
+
+    companion object {
+        fun newInstance(
+        ): LottieDialogFragment {
+            return LottieDialogFragment()
+        }
+    }
+
+    override fun show(manager: FragmentManager, tag: String?) {
+        val ft = manager.beginTransaction()
+        ft.add(this, tag)
+        ft.commitAllowingStateLoss()
+    }
+}
