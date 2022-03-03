541e96659ba98ce3222bb750506fef1ffa207fe6
kyusikkyu <kyusikkyu@gmail.com>
2022-03-03 14:44:11 +0900

    Error 처리 관련 코드 추가 - ErrorEntity - errorHandler (completable / single)

--- app/src/main/java/com/sokind/data/remote/base/BaseAPICallTransformer.kt
+++ app/src/main/java/com/sokind/data/remote/base/BaseAPICallTransformer.kt
@@ -1,7 +1,15 @@
 package com.sokind.data.remote.base
 
+import com.sokind.data.repository.token.TokenRepository
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.CompletableTransformer
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.core.SingleTransformer
+import retrofit2.HttpException
+
+fun Completable.transformCompletableToSingleDefault(): Single<BaseNetworkCallResult<Unit>> =
+    toSingleDefault(Unit)
+        .compose(wrappingSingleNetworkCallResult())
 
 fun <T> Single<T>.wrappingAPICallResult(): Single<BaseNetworkCallResult<T>> =
     compose(wrappingSingleNetworkCallResult())
@@ -11,4 +19,40 @@ private fun <T> wrappingSingleNetworkCallResult() =
         single
             .map { data -> BaseNetworkCallResult(data) }
             .onErrorReturn { BaseNetworkCallResult(throwable = it) }
+    }
+
+fun <T> errorHandlerSingle(tokenRepository: TokenRepository) =
+    SingleTransformer<T, T> { single ->
+        single
+            .retryWhen { error ->
+                return@retryWhen error
+                    .flatMapSingle {
+                        if (it is HttpException) {
+                            if (it.code() == 400) {
+                                return@flatMapSingle tokenRepository
+                                    .checkToken()
+                                    .andThen(Single.just(Unit))
+                            }
+                        }
+                        return@flatMapSingle Single.error(it)
+                    }
+            }
+    }
+
+fun errorHandlerCompletable(tokenRepository: TokenRepository) =
+    CompletableTransformer { completable ->
+        completable
+            .retryWhen { error ->
+                return@retryWhen error
+                    .flatMapSingle {
+                        if (it is HttpException) {
+                            if (it.code() == 401) {
+                                return@flatMapSingle tokenRepository
+                                    .checkToken()
+                                    .andThen(Single.just(Unit))
+                            }
+                        }
+                        return@flatMapSingle Single.error(it)
+                    }
+            }
     }
---
+++ app/src/main/java/com/sokind/data/remote/base/ErrorEntity.kt
@@ -0,0 +1,5 @@
+package com.sokind.data.remote.base
+
+object ErrorEntity {
+    object InvalidPw: Throwable()
+}
