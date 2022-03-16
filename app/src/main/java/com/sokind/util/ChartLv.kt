f4548224f85c2d231c5f4ce7717f87dd1c45a13e
kyusikkyu <kyusikkyu@gmail.com>
2022-03-16 18:31:22 +0900

    Lv 퍼센트 구간별 문구 적용

--- app/src/main/java/com/sokind/util/ChartLv.kt
+++ app/src/main/java/com/sokind/util/ChartLv.kt
@@ -26,23 +26,29 @@ class ChartLv(
         val progress = fin / total * 100
         val comment: String
         when(progress) {
-            in 0f..20f -> {
-                comment = "0~20"
+            0f -> {
+                comment = "쏘카인드로 서비스교육을 시작해볼까요?"
             }
-            in 21f..40f -> {
-                comment = "21~40"
+            in 1f..19f -> {
+                comment = "아직 쏘카인드 새내기입니다~!"
             }
-            in 41f..60f -> {
-                comment = "41~60"
+            in 20f..39f -> {
+                comment = "열심히 교육중이시네요. 화이팅!"
             }
-            in 61f..80f -> {
-                comment = "61~80"
+            in 40f..59f -> {
+                comment = "친절왕으로 달려가는 중 입니다."
             }
-            in 81f..100f -> {
+            in 60f..79f -> {
                 comment = "거의 다 왔어요! 조금만 힘내세요!"
             }
+            in 80f..99f -> {
+                comment = "끝이 보여요! 조금만 힘내세요!"
+            }
+            100f -> {
+                comment = "교육 끝~! 수고 많으셨습니다."
+            }
             else -> {
-                comment = "else"
+                comment = "쏘카인드로 서비스교육을 시작해볼까요?"
             }
         }
 
